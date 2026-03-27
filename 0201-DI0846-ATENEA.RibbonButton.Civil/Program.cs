b591ab7fd591e433f98dfdf144d0dd8bb402e06d
Pasico92 <pasico92@gmail.com>
2026-03-27 11:36:41 +0100

    first commit

---
+++ 0201-DI0846-ATENEA.RibbonButton.Civil/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0201_DI0846_ATENEA.RibbonButton.Civil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
